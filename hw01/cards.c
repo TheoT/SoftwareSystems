40edb0e2b8ecb2898ffc215e6680b39dedd44840
TheoT <theodore.thompson@students.olin.edu>
2014-01-28 19:27:16 -0500

    refactored cards.c

--- hw01/cards.c
+++ hw01/cards.c
@@ -1,37 +1,48 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+int get_card_val(char card_name[])
+{
+	switch(card_name[0]){
+		case 'K':
+		case 'Q':
+		case 'J':
+			return 10;
+			break;
+		case 'A':
+			return 11;
+			break;
+		default:
+			return atoi(card_name);
+		}
+}
+void adjust_count(int *count,int val)
+{
+	if (val>2 && val<7){
+		*count=*count+1;
+	} else if (val==10){
+		*count=*count-1;
+	}
+}
 int main(int argc, char const *argv[])
 {
 	char card_name[3];
 	int count = 0;
-	while(card_name[0]!='X'){
+	int run_flag=1;
+	while(run_flag){
 		puts("enter the card_name");
 		scanf("%2s",card_name);
-		int val=0;
-		switch(card_name[0]){
-		case 'K':
-		case 'Q':
-		case 'J':
-			val=10;
-			break;
-		case 'A':
-			val=11;
-			break;
-		case 'X':
+		if(card_name[0]=='X'){
+			run_flag=0;
+			puts("quitting");
 			continue;
-		default:
-			val=atoi(card_name);
-			if (val<1 || val>10){
-				puts("invalid input");
-				continue;
-			}
 		}
-		if (val>2 && val<7){
-			count++;
-		} else if (val==10){
-			count--;
+		int val=get_card_val(card_name);
+		if (val<1 || val>10){
+			puts("invalid input");
+			continue;
 		}
+		adjust_count(&count,val);
 		printf("current count: %i\n",count);
 	}
 	return 0;
